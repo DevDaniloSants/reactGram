0f3dcac00f2f00f59d24c8a76966ce343960c1af
Danilo Santos <dscofield@outlook.com.br>
2023-12-29 23:37:16 -0300

    Adiciona a função de login, a validação do mesmo e a rota

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 // Controller
-const { register } = require('../controllers/UserController');
+const { register, login } = require('../controllers/UserController');
 
 // middlewares
 const validate = require('../middlewares/handleValidation.js');
-const { userCreateValidation } = require('../middlewares/userValidation.js');
+const {
+  userCreateValidation,
+  loginValidation,
+} = require('../middlewares/userValidation.js');
+
 // Routes
 router.post('/register', userCreateValidation(), validate, register);
+router.post('/login', loginValidation(), validate, login);
 
 module.exports = router;

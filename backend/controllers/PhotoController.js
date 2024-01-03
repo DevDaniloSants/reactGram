ae621cbd2110a62485294b03b2fe86097b9fcc94
Danilo Santos <dscofield@outlook.com.br>
2024-01-03 18:54:40 -0300

    Adiciona a função e rota de atualizar uma foto

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -95,10 +95,43 @@ const getPhotoById = async (req, res) => {
   res.status(200).json(photo);
 };
 
+// update photo
+const updatePhoto = async (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const photo = await Photo.findById({ _id: id });
+
+  const reqUser = req.user;
+
+  // check if photo exists
+  if (!photo) {
+    res.status(404).json({ errors: ['foto não encontrada'] });
+    return;
+  }
+
+  // check if photo belongs to user
+  if (!photo.userId.equals(reqUser._id)) {
+    res.status(422).json({
+      errors: ['Ocorreu um erro, por favor tente novamente mais tarde'],
+    });
+    return;
+  }
+
+  if (title) {
+    photo.title = title;
+  }
+
+  await photo.save();
+
+  res.status(200).json({ photo, message: 'Foto atualizada com sucesso' });
+};
+
 module.exports = {
   insertPhoto,
   deletePhoto,
   getAllPhotos,
   getUserPhotos,
   getPhotoById,
+  updatePhoto,
 };

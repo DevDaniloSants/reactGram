f83027404bd72a617859e839ca035a6453668d3b
Danilo Santos <dscofield@outlook.com.br>
2024-01-03 17:11:31 -0300

    Adiciona a função e rota de buscar uma foto pelo seu id

--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -7,6 +7,7 @@ const {
   deletePhoto,
   getAllPhotos,
   getUserPhotos,
+  getPhotoById,
 } = require('../controllers/PhotoController');
 // middlewares
 const { photoInsertValidation } = require('../middlewares/photoValidation');
@@ -25,6 +26,7 @@ router.post(
 );
 router.delete('/:id', authGuard, deletePhoto);
 router.get('/', authGuard, getAllPhotos);
-router.get('/user/:id', authGuard, getUserPhotos)
+router.get('/user/:id', authGuard, getUserPhotos);
+router.get('/:id', authGuard, getPhotoById);
 
 module.exports = router;

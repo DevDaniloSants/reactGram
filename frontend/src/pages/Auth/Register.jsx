55490c505378429a38ab8c75eb04dc4b4e45274b
Danilo Santos <dscofield@outlook.com.br>
2024-01-05 18:55:19 -0300

    Adiciona o css das páginas de autenticação (login,register)

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,7 +1,34 @@
 import styles from './Auth.module.css';
 
+// Components
+import { Link } from 'react-router-dom';
+
+// Hooks
+import { useEffect, useState } from 'react';
+
 const Register = () => {
-  return <div>Register</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.register}>
+      <h2>Reactgram</h2>
+      <p className={styles.subtitle}>
+        Cadastre-se para ver as fotos dos seus amigos.
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a Psenha" />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem conta ? <Link to="/login">Clique Aqui</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Register;

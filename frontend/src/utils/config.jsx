a50b7b8eb7e826240c691f336e2cf84e6813e6a3
Danilo Santos <dscofield@outlook.com.br>
2024-01-08 20:03:26 -0300

    Revert "Cria o slice de autenticação"
    This reverts commit ae4eb02239eb2c30a3215c3f75a6a5edc20e5864.

--- frontend/src/utils/config.jsx
+++ frontend/src/utils/config.jsx
@@ -4,7 +4,7 @@ export const uploads = 'http://localhost:5000/uploads';
 export const requestConfig = (method, data, token = null, imagem = null) => {
   let config;
 
-  if (imagem) {
+  if (image) {
     config = {
       method,
       body: data,

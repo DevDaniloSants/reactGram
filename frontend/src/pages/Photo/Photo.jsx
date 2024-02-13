48a297cf70c28a56f7a5d8953d4837aafa1bc0f1
Danilo Santos <dscofield@outlook.com.br>
2024-02-13 10:16:04 -0300

    feat: Exibe o components de mensagem quando a foto é curtida e cria um custom hook para resetar a mensagem após 2 seg

--- frontend/src/pages/Photo/Photo.jsx
+++ frontend/src/pages/Photo/Photo.jsx
@@ -13,6 +13,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getPhotoById, like } from '../../slices/photoSlice';
 import LikeContainer from '../../components/LikeContainer/LikeContainer';
+import { useResetComponentMessage } from '../../hooks/useResetComponents';
 
 // Redux
 
@@ -20,6 +21,9 @@ const Photo = () => {
   const { id } = useParams();
 
   const dispatch = useDispatch();
+
+  const resetMessage = useResetComponentMessage(dispatch);
+
   const { user } = useSelector((state) => state.auth);
   const { photo, loading, error, message } = useSelector(
     (state) => state.photo,
@@ -32,6 +36,7 @@ const Photo = () => {
   // like and comments
   const handleLike = () => {
     dispatch(like(photo._id));
+    resetMessage();
   };
 
   if (loading) {
@@ -42,6 +47,10 @@ const Photo = () => {
     <div id={styles.photo}>
       <PhotoItem photo={photo} />
       <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+      <div className={styles.message_container}>
+        {error && <Message msg={error} type={'error'} />}
+        {message && <Message msg={message} type={'success'} />}
+      </div>
     </div>
   );
 };
